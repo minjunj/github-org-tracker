85e476362b4f75cd17235cd319f06198da7959c6
minjunj <minjun_jo@gm.gist.ac.kr>
2024-02-20 13:55:58 +0900

    first commit

--- st.py
+++ st.py
@@ -26,7 +26,7 @@ def github_json_parse(parse, repo_name, start_date, end_date):
             contributor = parse[con_num]['author']['login']
             data = parse[con_num]['weeks']
             result_dic[contributor] = 0
-            # Convert UNIX timestamps to YY-MM-DD format
+            
             for item in data:
                 if item['c'] != 0:
                     item['w'] = datetime.utcfromtimestamp(item['w']).strftime('%y%m%d')
@@ -41,7 +41,7 @@ def github_json_parse2(result_dic, result, parse, repo_name, start_date, end_dat
             contributor = parse[con_num]['author']['login']
             data = parse[con_num]['weeks']
             result_dic[contributor] = 0
-            # Convert UNIX timestamps to YY-MM-DD format
+        
             for item in data:
                 if item['c'] != 0:
                     item['w'] = datetime.utcfromtimestamp(item['w']).strftime('%y%m%d')
@@ -62,24 +62,18 @@ def specific_repo(org, repo_name, start_date_input, end_date_input, headers):
         original = github_json_parse(parse, repo_name, start_date, end_date)
     else:
         raise Exception("wrong date") 
-    #original = result
-    # Extract the first dictionary (assuming there's only one)
     data_dict = original[0]
-    # Filter out the pairs where the value is -1
     filtered_data = {k: v for k, v in data_dict.items() if v != -1}
-    # Create the DataFrame structure
     data = {'Categories': list(filtered_data.keys())}
     for category in data['Categories']:
-        # Initialize all values to 0, then set the diagonal value
         data[category] = [0 if cat != category else val for cat, val in filtered_data.items()]
-    # Create the DataFrame
+
     df = pd.DataFrame(data)
     df = df.set_index('Categories')
     return chart_placeholder.bar_chart(df.sum(axis=1), width=0.01)  # Aggregates the sum of all categories for each item
 #########################################################    
 
 def get_repo(org, headers):
-
     r = requests.get('https://api.github.com/orgs/'+str(org)+'/repos', headers=headers)
     parse = json.loads(r.text)
     repo_name_list = []
@@ -126,43 +120,33 @@ def overall(org, start_date_input, end_date_input, headers):
 
     filtered_data = {k: v for k, v in user_name_list.items() if v != -1}
 
-    # Simplified DataFrame creation
     df = pd.DataFrame.from_dict(filtered_data, orient='index', columns=['Contributions'])
-    # Assuming chart_placeholder.bar_chart is a placeholder for actual chart generation
-    # Replace with your actual chart generation logic
+
     return chart_placeholder.bar_chart(df.sum(axis=1), width=0.01)
 ############################################################
 
 st.title('Infoteam Tracker')
 org = os.getenv('ORG')
 
-# Get repository list and add 'Overall' option
 name_list = ['Overall'] + get_repo(org, headers)
 
-# User selection for repository view
 selection = st.selectbox('Select Repository view:', name_list)
 
-# Input form for start and end dates
 with st.form(key='date_range_form'):
     start_date = st.date_input("Start Date", value=datetime(2022, 1, 1), min_value=datetime(2000, 1, 1), max_value=datetime.now())
     end_date = st.date_input("End Date", value=datetime(2024, 2, 18), min_value=datetime(2000, 1, 1), max_value=datetime.today())
     submit_button = st.form_submit_button(label='Submit')
 
-# Placeholder for the bar chart
 chart_placeholder = st.empty()
 
-# Check if the dates are valid
 if submit_button:
     if start_date < end_date:
-        # Format dates as strings if necessary
         start_date_str = start_date.strftime('%y-%m-%d')
         end_date_str = end_date.strftime('%y-%m-%d')
 
-        # Conditional logic to call the correct function based on the selection
         if selection == 'Overall':
             overall(org, start_date_str, end_date_str, headers)
         else:
-            # Call specific_repo with the selected repository name
             specific_repo(org, selection, start_date_str, end_date_str, headers)
     else:
         st.error("Error: End date must be after start date.")
